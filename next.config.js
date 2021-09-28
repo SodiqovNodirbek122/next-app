4434d873c2c027599f8ecfae91195207081600c7
unknown <nodirbeksodiqov122@gmail.com>
2021-09-28 14:56:24 +0500

    next app

--- next.config.js
+++ next.config.js
@@ -1,22 +1,3 @@
-const { nextI18NextRewrites } = require('next-i18next/rewrites')
-// const runtimeCaching = require('next-pwa/cache')
-// const withPWA = require('next-pwa')
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-})
-
-const localeSubpaths = {
-    ru: 'ru',
-    en: 'en',
+module.exports = {
+  reactStrictMode: true,
 }
-module.exports = withBundleAnalyzer({
-    webpack5: true,
-    pwa: {
-        dest: 'public',
-        swSrc: 'service-worker.js',
-    },
-    rewrites: async () => nextI18NextRewrites(localeSubpaths),
-    publicRuntimeConfig: {
-        localeSubpaths,
-    },
-})
